ed34ae63a92703c436c5f3c0df3ea0c757333336
Ryconler <1290279000@qq.com>
2019-02-15 13:32:30 +0800

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
     script: 'bin/www',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
     instances: 1,
     autorestart: true,
     watch: true,
