7d2e9ca9944ca961f903144e47c005fcbac95fa4
Ryconler <1290279000@qq.com>
2019-04-02 16:07:40 +0800

    a

--- config/waCode.js
+++ config/waCode.js
@@ -4,7 +4,7 @@ _wa.push(['config', '${config}']);
 (function() {
 var newScript = document.createElement('script');
 newScript.async = true;
-newScript.src = 'http://localhost:4000/resources/javascripts/wa.js';
+newScript.src = 'http://analytics.server.jessezhu.cn/resources/javascripts/wa.js';
 var firstScript = document.getElementsByTagName('script')[0];
 firstScript.parentNode.insertBefore(newScript, firstScript);
 })();`;
