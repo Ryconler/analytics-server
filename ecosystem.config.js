bb8b87afbb5aa53970e15ff35edb516d6ba3ca98
Ryconler <1290279000@qq.com>
2019-04-03 19:54:59 +0800

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,8 @@ module.exports = {
   apps : [{
     name: 'analytics-server',
     script: 'bin/www',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    instances: 1,
-    autorestart: true,
     watch: true,
-    max_memory_restart: '1G',
+    ignore_watch: ['node_modules'],
     env: {
       NODE_ENV: 'production'
     },
