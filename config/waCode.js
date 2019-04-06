3d483553ba5db636f595919a8543c733744c883d
Ryconler <1290279000@qq.com>
2019-04-06 10:31:08 +0800

    a

--- config/waCode.js
+++ config/waCode.js
@@ -1,12 +1,9 @@
 /* 检验代码是否安装时爬虫使用 */
 module.exports = function (config) {
-    return `var _wa= _wa || [ ];
-_wa.push(['config', '${config}']);
-(function() {
+    return `_wa.push(['config', '${config}']);
 var newScript = document.createElement('script');
 newScript.async = true;
 newScript.src = 'http://analytics.server.jessezhu.cn/resources/javascripts/wa.js';
 var firstScript = document.getElementsByTagName('script')[0];
-firstScript.parentNode.insertBefore(newScript, firstScript);
-})();`;
+firstScript.parentNode.insertBefore(newScript, firstScript);`
 };
