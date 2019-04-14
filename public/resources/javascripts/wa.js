b2a2079283af739ad79d1e22713904e323845e94
Ryconler <1290279000@qq.com>
2019-04-14 20:08:44 +0800

    a

--- public/resources/javascripts/wa.js
+++ public/resources/javascripts/wa.js
@@ -1,6 +1,6 @@
 (function () {
-    // const server = 'http://analytics.server.jessezhu.cn'
-    const server = 'http://127.0.0.1:4000'
+    const server = 'http://analytics.server.jessezhu.cn'
+    // const server = 'http://127.0.0.1:4000'
 
     /* cookie */
     function setCookie(key, value) {
