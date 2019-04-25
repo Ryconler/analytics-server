7aeba34e0eee8dd40d5b6229ba5a51c9f260aa34
Ryconler <1290279000@qq.com>
2019-04-25 10:43:07 +0800

    a

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     database: 'analytics',
     username: 'root',
-    password: process.env.NODE_ENV === 'production' ? 'ntu2015' : 'root',
+    password: 'root',
     host: '127.0.0.1',
     port: 3306
 }
