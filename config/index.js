e21f06befda9e3d6116dac0b06a2419c02604a44
Ryconler <1290279000@qq.com>
2019-09-01 20:50:32 +0800

    a

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@ const devFlag = process.env.NODE_ENV !== 'production';
 module.exports = {
     devFlag,
     baseURL: devFlag ? 'http://localhost:8080' : 'http://analytics.jessezhu.cn',
-    port: 5000
+    port: 4000
 }
