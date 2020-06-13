40a23065a0f2152068d207525edfd9518f2f8ab7
朱星杰 <1290279000@qq.com>
2020-06-13 13:52:02 +0800

    修复淘宝ip查询接口失效带来的报错

--- utils/ApiUtil.js
+++ utils/ApiUtil.js
@@ -1,6 +1,9 @@
 const request = require('request')
 
 module.exports.getIpInfo = function (ip, callback) {
+    callback && callback({});
+    return;
+    // 淘宝ip查询接口已失效
     request('http://ip.taobao.com/service/getIpInfo.php?ip=' + ip, function (error, response, body) {
         /*判断请求是否成功*/
         if (!error && response.statusCode === 200) {
