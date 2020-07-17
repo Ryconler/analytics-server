052c1fa2658914c6f564cb6e517f45fbc1b71aec
朱星杰 <1290279000@qq.com>
2020-07-17 21:45:29 +0800

    配置更改

--- config/index.js
+++ config/index.js
@@ -2,6 +2,5 @@ const devFlag = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     devFlag,
-    baseURL: devFlag ? 'http://localhost:8080' : 'http://analytics.jessezhu.cn',
     port: 4000
 }
--- config/waCode.js
+++ config/waCode.js
@@ -1,8 +1,6 @@
 /* 检验代码是否安装时爬虫使用 */
-const baseURL = require('../config/index').baseURL;
-
 module.exports = function (config) {
-    return `<script async src="${baseURL}/resources/javascripts/wa.js"></script>
+    return `<script async src="http://analytics.wzmxx.com/resources/javascripts/wa.js"></script>
 <script>
     window.waData = window.waData || [];
     function waTag(){waData.push(arguments);}
