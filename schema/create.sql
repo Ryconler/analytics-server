f0c31cdd3772e4b8c98b9a33b1166653a2cf9357
朱星杰 <1290279000@qq.com>
2021-03-18 01:18:33 +0800

    初始化sql

--- schema/create.sql
+++ schema/create.sql
@@ -1,25 +1,37 @@
-/*
- Navicat Premium Data Transfer
+DROP TABLE IF EXISTS `website`;
+CREATE TABLE `website` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_id` int(11) DEFAULT NULL,
+  `config` varchar(255) DEFAULT NULL,
+  `host` varchar(255) DEFAULT NULL,
+  `index_url` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `validate` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50727
- Source Host           : localhost:3306
- Source Schema         : analytics
 
- Target Server Type    : MySQL
- Target Server Version : 50727
- File Encoding         : 65001
+DROP TABLE IF EXISTS `record`;
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `config` varchar(255) DEFAULT NULL,
+  `open_time` timestamp NULL DEFAULT NULL,
+  `close_time` timestamp NULL DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `service` varchar(255) DEFAULT NULL,
+  `referrer` varchar(255) DEFAULT NULL,
+  `wxh` varchar(255) DEFAULT NULL,
+  `depth` varchar(10) DEFAULT NULL,
+  `device` varchar(10) DEFAULT NULL,
+  `os` varchar(255) DEFAULT NULL,
+  `browser_name` varchar(255) DEFAULT NULL,
+  `browser_version` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
- Date: 13/06/2020 13:05:28
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for custom
--- ----------------------------
 DROP TABLE IF EXISTS `custom`;
 CREATE TABLE `custom` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,75 +43,36 @@ CREATE TABLE `custom` (
   `value` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `ip` varchar(255) DEFAULT NULL,
-  `time` varchar(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Table structure for record
--- ----------------------------
-DROP TABLE IF EXISTS `record`;
-CREATE TABLE `record` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `config` varchar(255) DEFAULT NULL,
-  `open_time` varchar(20) DEFAULT NULL,
-  `close_time` varchar(20) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `urls` varchar(2550) DEFAULT NULL,
-  `open_times` varchar(2550) DEFAULT NULL,
-  `ip` varchar(50) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `service` varchar(255) DEFAULT NULL,
-  `referrer` varchar(255) DEFAULT NULL,
-  `wxh` varchar(10) DEFAULT NULL,
-  `depth` varchar(10) DEFAULT NULL,
-  `device` varchar(10) DEFAULT NULL,
-  `os` varchar(255) DEFAULT NULL,
-  `browser_name` varchar(255) DEFAULT NULL,
-  `browser_version` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `register_date` varchar(20) DEFAULT NULL,
+  `register_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Table structure for website
--- ----------------------------
-DROP TABLE IF EXISTS `website`;
-CREATE TABLE `website` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `u_id` int(11) DEFAULT NULL,
-  `config` varchar(255) DEFAULT NULL,
-  `host` varchar(255) DEFAULT NULL,
-  `index_url` varchar(255) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `create_date` varchar(20) DEFAULT NULL,
-  `validate` char(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `user` VALUES (1, 'admin', '$2a$10$bjKNpuUn397uooCfV/BQHOwcN6jGohMsTXFLbfWPTzh6.QHUZGxi2', 'zxj@wzmxx.com', now());
+INSERT INTO `website` VALUES (1, 1, 'WA-LNH5FK-1', 'analytics.wzmxx.com', 'http://analytics.wzmxx.com', 'JesseZhu的用户行为分析平台', now(), '0');
+INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `create_time`) VALUES (1, 'WA-LNH5FK-1', 'event', '登录按钮', '点击', '登录', NULL, 'http://analytics.wzmxx.com/login', '101.245.241.102', now());
+INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `create_time`) VALUES (2, 'WA-LNH5FK-1', 'conversion', '注册转化', '注册页面', '1', NULL, 'http://analytics.wzmxx.com/register', '101.245.241.102', now());
+INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `create_time`) VALUES (3, 'WA-LNH5FK-1', 'conversion', '注册转化', '注册页面', '1', NULL, 'http://analytics.wzmxx.com/register', '101.245.241.102', now());
+INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `create_time`) VALUES (4, 'WA-LNH5FK-1', 'conversion', '注册转化', '注册成功', '2', NULL, 'http://analytics.wzmxx.com/register', '101.245.241.102', now());
+INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `create_time`) VALUES (5, 'WA-LNH5FK-1', 'event', '登录按钮', '点击', '登录', NULL, 'http://analytics.wzmxx.com/login', '101.245.241.102', now());
+INSERT INTO `record`(`id`, `config`, `open_time`, `close_time`, `url`, `ip`, `address`, `service`, `referrer`, `wxh`, `depth`, `device`, `os`, `browser_name`, `browser_version`) 
+  VALUES (1, 'WA-LNH5FK-1', now(), now(), 'http://analytics.wzmxx.com/login', '101.245.241.102', NULL, NULL, '', '1440x900', '30', 'pc', 'Mac/iOS', 'Chrome', NULL);
+INSERT INTO `record`(`id`, `config`, `open_time`, `close_time`, `url`, `ip`, `address`, `service`, `referrer`, `wxh`, `depth`, `device`, `os`, `browser_name`, `browser_version`)
+  VALUES (2, 'WA-LNH5FK-1', now(), now(), 'http://localhost:8080/', '101.245.241.102', NULL, NULL, '', '1440x900', '30', 'pc', 'Mac/iOS', 'Chrome', NULL);
 
-
-INSERT INTO `user` VALUES (1, 'zhuxingjie', '$2a$10$bjKNpuUn397uooCfV/BQHOwcN6jGohMsTXFLbfWPTzh6.QHUZGxi2', 'ryconler@icloud.com', '2020-06-13 12:18:12');
-INSERT INTO `website` VALUES (1, 1, 'WA-LNH5FK-1', 'analytics.wzmxx.com', 'http://analytics.wzmxx.com', 'JesseZhu的用户行为分析平台', '2020-06-13 12:29:34', '0')
-INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `time`) VALUES (1, 'WA-LNH5FK-1', 'event', '登录按钮', '点击', '登录', NULL, 'http://analytics.wzmxx.com/login', '101.245.241.102', '1592027918931');
-INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `time`) VALUES (2, 'WA-LNH5FK-1', 'conversion', '注册转化', '注册页面', '1', NULL, 'http://analytics.wzmxx.com/register', '101.245.241.102', '1592027918931');
-INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `time`) VALUES (3, 'WA-LNH5FK-1', 'conversion', '注册转化', '注册页面', '1', NULL, 'http://analytics.wzmxx.com/register', '101.245.241.102', '1592027918931');
-INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `time`) VALUES (4, 'WA-LNH5FK-1', 'conversion', '注册转化', '注册成功', '2', NULL, 'http://analytics.wzmxx.com/register', '101.245.241.102', '1592027918931');
-INSERT INTO `custom`(`id`, `config`, `track`, `category`, `action`, `label`, `value`, `url`, `ip`, `time`) VALUES (5, 'WA-LNH5FK-1', 'event', '登录按钮', '点击', '登录', NULL, 'http://analytics.wzmxx.com/login', '101.245.241.102', '1592027918931');
-INSERT INTO `record`(`id`, `config`, `open_time`, `close_time`, `url`, `urls`, `open_times`, `ip`, `address`, `service`, `referrer`, `wxh`, `depth`, `device`, `os`, `browser_name`, `browser_version`) 
-  VALUES (1, 'WA-LNH5FK-1', '1592027918931', '1592029116717', 'http://analytics.wzmxx.com/login', 'http://analytics.wzmxx.com/login,http://analytics.wzmxx.com/analytics', '1592027918931,1592028080219', '101.245.241.102', NULL, NULL, '', '1440x900', '30', 'pc', 'Mac/iOS', 'Chrome', NULL);
-INSERT INTO `record`(`id`, `config`, `open_time`, `close_time`, `url`, `urls`, `open_times`, `ip`, `address`, `service`, `referrer`, `wxh`, `depth`, `device`, `os`, `browser_name`, `browser_version`)
-  VALUES (2, 'WA-LNH5FK-1', '1592028849623', '1592029237155', 'http://localhost:8080/', 'http://localhost:8080/', '1592028849623', '101.245.241.102', NULL, NULL, '', '1440x900', '30', 'pc', 'Mac/iOS', 'Chrome', NULL);
+CREATE EVENT `analytics`.`无标题`
+ON SCHEDULE
+EVERY '200' MINUTE
+DO INSERT INTO record (config,open_time,close_time,url,ip,wxh,depth,device,os,browser_name)
+VALUES('WA-LNH5FK-1',NOW(),NOW(),'http://analytics.jessezhu.cn/login','222.66.77.226','1440x900','30','pc','Mac/iOS','Chrome');
+SET GLOBAL event_scheduler = ON;
